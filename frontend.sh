cd8ee5573f54d238fd86c4ceabfc411338d32ef4
shujath-git900 <shujath.devops@gmail.com>
2026-05-05 17:23:37 +0530

    copy nginx config file

--- frontend.sh
+++ frontend.sh
@@ -2,6 +2,8 @@ dnf module disable nginx -y
 dnf module enable nginx:1.26 -y
 dnf install -y nginx
 
+cp nginx.conf /etc/nginx/nginx.conf
+
 curl -fsSL https://rpm.nodesource.com/setup_22.x | bash -
 dnf install -y nodejs
 
